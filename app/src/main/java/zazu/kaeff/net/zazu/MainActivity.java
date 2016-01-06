0b37eabfb04df9d25bad5b8cdb18dd42bc8ded42
kaeff <hi@kaeff.net>
2016-01-06 01:44:53 +0100

    Spike launching LogTimeActivity via Notification

--- app/src/main/java/zazu/kaeff/net/zazu/MainActivity.java
+++ app/src/main/java/zazu/kaeff/net/zazu/MainActivity.java
@@ -1,7 +1,12 @@
 package zazu.kaeff.net.zazu;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -43,12 +48,38 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void pickDate(View view) {
+    @NonNull
+    private Intent createLogTimeIntendForSelectedDate() {
         DatePicker datePicker = (DatePicker) findViewById(R.id.logTimeDatePicker);
         Date date = new Date(datePicker.getCalendarView().getDate());
 
         Intent intent = new Intent(this, LogTimeActivity.class);
         intent.putExtra(LogTimeActivity.EXTRA_DATE, date);
-        startActivity(intent);
+        return intent;
+    }
+
+    public void pickDate(View view) {
+        startActivity(createLogTimeIntendForSelectedDate());
+    }
+
+    public void showNotification(View view) {
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(LogTimeActivity.class);
+        stackBuilder.addNextIntent(createLogTimeIntendForSelectedDate());
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(android.R.drawable.ic_lock_idle_alarm)
+                .setContentTitle("Zazu")
+                .setContentText("Tap to log beginning of day")
+                .setContentIntent(resultPendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0, builder.build());
     }
 }
