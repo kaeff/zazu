3810d864e8717833890602f60c5d7b4efef84a0a
kaeff <hi@kaeff.net>
2016-01-13 01:08:18 +0100

    Add activity for displaying time logs

--- app/src/main/java/net/kaeff/zazu/DatabaseHelper.java
+++ app/src/main/java/net/kaeff/zazu/DatabaseHelper.java
@@ -2,9 +2,14 @@ package net.kaeff.zazu;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
+
+import org.joda.time.LocalDateTime;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
@@ -38,5 +43,16 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         values.put(DAY, timeLog.getDateTime().toString());
         values.put(TYPE, timeLog.getType().name());
         getWritableDatabase().insert(ACTIVITY_LOG, null, values);
+    }
+
+    public List<TimeLog> queryRecords() {
+        List<TimeLog> result = new LinkedList<>();
+        Cursor query = getReadableDatabase().query(ACTIVITY_LOG, new String[]{DAY, TYPE}, null, null, null, null, DAY);
+        while (query.moveToNext()) {
+            LocalDateTime dateTime = LocalDateTime.parse(query.getString(query.getColumnIndex(DAY)));
+            TimeLog.Type type = TimeLog.Type.valueOf(query.getString(query.getColumnIndex(TYPE)));
+            result.add(new TimeLog(dateTime, type));
+        }
+        return result;
     }
 }
---
+++ app/src/main/java/net/kaeff/zazu/ShowTimeLogsActivity.java
@@ -0,0 +1,58 @@
+package net.kaeff.zazu;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.joda.time.LocalDateTime;
+
+import java.util.List;
+
+import zazu.kaeff.net.zazu.R;
+
+public class ShowTimeLogsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_time_logs);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+
+    protected void onStart() {
+        super.onStart();
+
+        DatabaseHelper databaseHelper = new DatabaseHelper(this);
+        printTimeLogs(databaseHelper.queryRecords());
+    }
+
+    private void printTimeLogs(List<TimeLog> timeLogs) {
+        LinearLayout timeLogsContainer = (LinearLayout) findViewById(R.id.timeLogsContainer);
+        for (TimeLog timeLog : timeLogs) {
+            TextView textView = new TextView(this);
+            textView.setText(printTimeLog(timeLog));
+            timeLogsContainer.addView(textView);
+        }
+    }
+
+    private String printTimeLog(TimeLog timeLog) {
+        return timeLog.toString();
+    }
+}
